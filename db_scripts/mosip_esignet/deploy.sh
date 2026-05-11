e9711fed10060dc496f05a2fc55a35dd34f797a7
syed salman <72004356+syedsalman3753@users.noreply.github.com>
2026-05-11 16:40:29 +0530

    Update deploy.sh
    Signed-off-by: syed salman <72004356+syedsalman3753@users.noreply.github.com>

--- db_scripts/mosip_esignet/deploy.sh
+++ db_scripts/mosip_esignet/deploy.sh
@@ -29,7 +29,7 @@ echo "Terminated connections"
 #PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -f role_dbuser.sql -v dbuserpwd=\'$DBUSER_PWD\' 
 
 ## Create DB
-PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -f db.sql 
+#PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -f db.sql 
 PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -f ddl.sql 
 
 ## Grants
